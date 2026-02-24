9b4f2c46ee1b38f40156b20ae6795fd475e1940a
я <v9017352571@gmail.com>
2026-02-24 11:11:25 +0300

    2.9 Мердж всего.

--- menu.py
+++ menu.py
@@ -613,3 +613,19 @@ class FinalResultsView(arcade.View):
             else:
                 arcade.draw_rectangle_filled(x, y, 8, 8, color)
         self.ui_manager.draw()
+
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Заводы и Тауэр Дефенс"
+
+
+def main():
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    start_view = StartMenuView()
+    window.show_view(start_view)
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
